cb8122c99ce2f920f1ba24824b9325d91d17f910
Stanislav-Vyhnan <stas.vignan@gmail.com>
2022-01-17 15:42:00 +0200

    feat(): changed some things

--- back-end/middleware/deleteImg.js
+++ back-end/middleware/deleteImg.js
@@ -3,7 +3,6 @@ import fs from 'fs';
 const deleteImg = (req, file, cb) => {
   const fileName = req.query[0];
   fs.unlink(`images/${fileName}`, function (err) {
-    console.log(req.params);
     if (err) {
       console.log(err);
     } else {
